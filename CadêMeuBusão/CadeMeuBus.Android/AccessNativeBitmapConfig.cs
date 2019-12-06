0b097fc3cf252afc17d46c35ae0355bfb5b662b6
ivanaugusto09 <55998990+ivanaugusto09@users.noreply.github.com>
2019-12-06 16:18:15 -0300

    Add files via upload

---
+++ CadêMeuBusão/CadeMeuBus.Android/AccessNativeBitmapConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.GoogleMaps.Android.Factories;
+using AndroidBitmapDescriptor = Android.Gms.Maps.Model.BitmapDescriptor;
+using AndroidBitmapDescriptorFactory = Android.Gms.Maps.Model.BitmapDescriptorFactory;
+
+namespace CadeMeuBus.Droid
+{
+    public sealed class AccessNativeBitmapConfig : IBitmapDescriptorFactory
+    {
+        public AndroidBitmapDescriptor ToNative(BitmapDescriptor descriptor)
+        {
+            int resId = 0;
+            switch (descriptor.Id)
+            {
+                case "type1":
+                    resId = Resource.Drawable.terminal1;
+                    break;
+                case "type2":
+                    resId = Resource.Drawable.pinBus2;
+                    break;
+                case "type3":
+                    resId = Resource.Drawable.bus1;
+                    break;
+            }
+
+            return AndroidBitmapDescriptorFactory.FromResource(resId);
+        }
+    }
+}
---
+++ CadêMeuBusão/CadeMeuBus.Android/BD/CaminhoDoBanco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CadeMeuBus.Droid.BD;
+using CadeMeuBus.BD;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(CaminhoDoBanco))]
+namespace CadeMeuBus.Droid.BD
+{
+    public class CaminhoDoBanco : I_LocalDados
+    {
+        public string getCaminho(string nomedoarquivo)
+        {
+            string path = System.Environment.GetFolderPath(
+            System.Environment.SpecialFolder.Personal);
+            string arquivo = Path.Combine(path, nomedoarquivo);
+            return arquivo;
+        }
+    }
+}
